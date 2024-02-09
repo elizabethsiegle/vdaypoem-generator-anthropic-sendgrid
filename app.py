9a8a6f5cc4a1701ae86bc852dc3ebd7f857f0b48
elizabethsiegle <lsiegle@twilio.com>
2024-02-09 14:49:41 -0800

    Replicate llama model option

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import dotenv_values
 from exa_py import Exa
 import os
 from PIL import Image
+import replicate
 import requests
 import sendgrid
 from sendgrid import SendGridAPIClient
@@ -24,7 +25,7 @@ anthropic = Anthropic(
 
 def main():
     st.title("Love Poem Generator❤️ 💌") 
-    st.write("Built w/ Anthropic, SendGrid, Streamlit, Exa, and Replit") 
+    st.write("Built w/ Anthropic, SendGrid, Streamlit, Exa, Replicate, and Replit") 
     image = Image.open('pikalove.png')
     st.image(image)
     
@@ -34,6 +35,9 @@ def main():
     """
     What do they like? What do you like about them? What's a funny memory with them?
     """)
+    model_toggle = st.radio("What LLM would you like to use", # lol it rhymes
+            [":rainbow[llama-2-70b-chat]", "***Claude***"],
+            captions = ["Hosted on Replicate", "Thank you, Anthropic"]) 
 
     addons = st.multiselect(
         'What would you like your love letter to include',
@@ -43,44 +47,82 @@ def main():
     st.write('You selected:', addons)
 
     user_email = st.text_input("Email to send love poem to📧", "lol@gmail.com (you deserve one too!)")
+    poem = ''
 
-    if st.button('Generate love poem w/ AI 🧠🤖'):
+    if st.button('Generate a poem w/ AI 🧠🤖'):
         with st.spinner('Processing📈...'):
             PROMPT1 = f"""
-            You are a copy editor. Edit the following blurb, ensuring the only pronouns used are "I": {receiver_description}. 
-            Next, parse out the items from the following string so it's only a list of items and no brackets or quotes: {addons}
-            Return only the edited blurb and the list of items and nothing else.
+                You are a copy editor. Edit the following blurb and return only that edited blurb, ensuring the only pronouns used are "I": {receiver_description}. 
+                Next, parse out the items from the following string so it's only a list of items and no brackets or quotes: {addons}
+                Add onto the edited blurb the list of items and nothing else. There should be no preamble.
             """
-            completion1 = anthropic.completions.create(
-                model="claude-2.1",
-                max_tokens_to_sample=700,
-                prompt=f"{HUMAN_PROMPT}: {PROMPT1}. {AI_PROMPT}",
-            )
-            print(completion1.completion)
-            newPronouns = completion1.completion
+            if model_toggle == "***Claude***":
+                completion1 = anthropic.completions.create(
+                    model="claude-instant-1.2", # claude-2.1
+                    max_tokens_to_sample=700,
+                    prompt=f"{HUMAN_PROMPT}: {PROMPT1}. {AI_PROMPT}",
+                )
+                print(completion1.completion)
+                newPronouns = completion1.completion
 
-            PROMPT= f"""
-            Please make me laugh by writing a short, silly, lighthearted, complimentary, lovey-dovey poem about the following person named {receiver_name}. 
-            <receiver_description>{newPronouns}</receiver_description>. 
-            I would enjoy it if the poem also jokingly included some of the following: {addons}. 
-            Return only the poem and nothing else.
-            """
-            print(PROMPT)
-            
-            
-            completion = anthropic.completions.create(
-                model="claude-2.1",
-                max_tokens_to_sample=1000,
-                prompt=f"{HUMAN_PROMPT}: {PROMPT}. {AI_PROMPT}",
-            )
-            poem = completion.completion
-            print(poem)
-            st.write(f'The generated poem: {poem}')
+                PROMPT= f"""
+                Please make me laugh by writing a short, silly, lighthearted, complimentary, lovey-dovey poem about the following person named {receiver_name}. 
+                <receiver_description>{newPronouns}</receiver_description>. 
+                I would enjoy it if the poem also jokingly included some of the following: {addons}. 
+                Return only the poem and nothing else.
+                """
+                print(PROMPT)
+                
+                completion = anthropic.completions.create(
+                    model="claude-2.1",
+                    max_tokens_to_sample=1000,
+                    prompt=f"{HUMAN_PROMPT}: {PROMPT}. {AI_PROMPT}",
+                )
+                poem = completion.completion
+                print(poem)
+                st.write(f'Generated poem:  {poem}')
+
+            elif model_toggle == ":rainbow[llama-2-70b-chat]":
+                editpronouns = replicate.run(
+                    "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+                    input={
+                        "prompt": PROMPT1,
+                        "max_new_tokens": 700
+                    }
+                )
+                newpronounsblurb = ''
+                for item in editpronouns:
+                    newpronounsblurb+=item
+                    print(item, end="")
+                print("newpronounsblurb ", newpronounsblurb)
+
+                PROMPT2= f"""
+                Please make me laugh by writing a short, silly, lighthearted, complimentary, lovey-dovey poem about the following person named {receiver_name}. 
+                <receiver_description>{newpronounsblurb}</receiver_description>. 
+                I would enjoy it if the poem also jokingly included some of the following: {addons}. 
+                Return only the poem and nothing else.
+                """
+                print(PROMPT2)
+
+                poem = replicate.run(
+                    "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+                    input={
+                        "prompt": PROMPT2,
+                        "max_new_tokens": 1000
+                    }
+                )
+                newpoem = ''
+                for item in poem:
+                    newpoem+=item
+                    print(item, end="")
+                print("newpoem ", newpoem)
+                st.write(f'The generated poem: {newpoem}')
+
 
             message = Mail(
-                from_email='poem@love.com',
+                from_email='love@poem.com',
                 to_emails=user_email,
-                subject='A poem for you!',
+                subject='A personal poem for you!❤️',
                 html_content=f'{poem}'
             )
 
